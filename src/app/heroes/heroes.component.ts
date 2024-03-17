05b8431d46635e513d3df08a75513726170b56f2
stdbob <bobber1@yahoo.com>
2024-03-17 16:52:47 +0200

    Show the hero object

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-heroes',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.css'
+})
+export class HeroesComponent {
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+}
