b735924392561a2f78883304aa5df532dbff1a2c
stdbob <bobber1@yahoo.com>
2024-03-17 18:45:35 +0200

    Create the HeroService

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {
   NgFor,
   UpperCasePipe,
@@ -6,8 +6,8 @@ import {
 import {FormsModule} from '@angular/forms';
 
 import {Hero} from '../hero';
-import {HEROES} from '../mock-heroes';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+import { HeroService } from '../hero.service';
 
 @Component({
   standalone: true,
@@ -21,11 +21,21 @@ import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
   ],
 })
 
-export class HeroesComponent {
-  heroes = HEROES;
+export class HeroesComponent implements OnInit {
+  heroes: Hero[] = [];
   selectedHero?: Hero;
+
+  constructor(private heroService: HeroService) {} //Reserve the constructor for minimal initialization such as wiring constructor parameters to properties. 
+  
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();//While you could call getHeroes() in the constructor, that's not the best practice.
   }
 }
