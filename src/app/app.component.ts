d1a92ce16be6c79216696f66d9e08b239672c470
stdbob <bobber1@yahoo.com>
2024-03-18 19:51:07 +0200

    Add RouterOutlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { HeroesComponent } from './heroes/heroes.component';
 import { MessagesComponent } from "./messages/messages.component";
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
     standalone: true,
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [HeroesComponent, MessagesComponent]
+    imports: [MessagesComponent,RouterOutlet,RouterLink]
 })
 export class AppComponent {
   title = 'Tour of Heroes';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: 'heroes', component: HeroesComponent }];
